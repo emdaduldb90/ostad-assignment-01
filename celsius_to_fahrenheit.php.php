d63849be7aeae771fe462022906f5962ab3ef4c8
Emdadul <emdaduldb90@gmail.com>
2024-07-02 22:23:35 +0600

    Assigment one done

---
+++ celsius_to_fahrenheit.php.php
@@ -0,0 +1,6 @@
+<?php
+$temperature = 32;
+
+$celsius = ($temperature * 9/5) + 32;
+
+printf("%.2f", $celsius);
---
+++ largest_number.php
@@ -0,0 +1,21 @@
+<?php
+$num1 = 4;
+$num2 = 5;
+$num3 = 6;
+
+// Define the Large numbers Method 1
+$largestNumber = max($num1, $num2, $num3);
+
+echo "Here are $largestNumber is largest number";
+
+echo "<br/><br/>";
+
+// Define the Large numbers Method 2
+if ($num1 >= $num2 && $num1 >= $num3) {
+    $largest = $num1;
+} elseif ($num2 >= $num1 && $num2 >= $num3) {
+    $largest = $num2;
+} else {
+    $largest = $num3;
+}
+echo "Largest among of number $num1, $num2, and $num3. Here are $largest is largest number";
