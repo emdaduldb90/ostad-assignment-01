a2b7625ba05ffa35921489014920e96504c57ec3
Emdadul <emdaduldb90@gmail.com>
2024-07-02 22:39:52 +0600

    ihave change somthing

--- largest_number.php
+++ largest_number.php
@@ -3,12 +3,23 @@ $num1 = 4;
 $num2 = 5;
 $num3 = 6;
 
+echo "<br/><br/>";
+
+echo "<b>Method 1:</b>";
+echo "<br/>";
 // Define the Large numbers Method 1
 $largestNumber = max($num1, $num2, $num3);
 
 echo "Here are $largestNumber is largest number";
 
 echo "<br/><br/>";
+
+
+
+
+
+echo "<b>Method 2:</b>";
+echo "<br/>";
 
 // Define the Large numbers Method 2
 if ($num1 >= $num2 && $num1 >= $num3) {
